25667b8fde6ae1d40d63cc3276e3f1f766c6b6e6
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2011-04-27 14:41:15 -0700

    Changing paths to run on Raichu and Linux

--- som/do_all_runs.py
+++ som/do_all_runs.py
@@ -1,9 +1,20 @@
 """Script to do all the new runs"""
 from runSom import bestMap
+import sys
 
-somPakF='/home/dani/repos/som_pak-3.1/'
+if sys.platform = 'darwin':
+    somPakF='/Users/dani/repos/som_pak-3.1/'
+    data = '/Users/dani/Desktop/WDlandGDF_processed/wbALL_no_emptiesZ.dat'
+    data = '/Users/dani/Desktop/WDlandGDF_processed/wbALL_no_empties_short_mini.dat'
 
-data = '/home/dani/AAA/LargeData/WDlandGDF_processed/wbALL_no_emptiesZ.dat'
+    nameO='/Users/dani/Desktop/AAA/LargeData/WDlandGDF_processed/som1'
+
+elif sys.platform = 'linux':
+    somPakF='/home/dani/repos/som_pak-3.1/'
+    data = '/home/dani/AAA/LargeData/WDlandGDF_processed/wbALL_no_emptiesZ.dat'
+
+    nameO='/home/dani/AAA/LargeData/WDlandGDF_processed/som1'
+
 
 runs = 1
 
@@ -12,7 +23,6 @@ datIn = data
 cols,rows,topo,neigh=map(str,[1500, 1500,'hexa','gaussian'])
 rlen1,alpha1,radious1=map(str,[10000,0.04,750])
 rlen2,alpha2,radious2=map(str,[100000,0.03,500])
-nameO='/home/dani/AAA/LargeData/WDlandGDF_processed/som1'
 
 bestMap(runs, somPakF,datIn,cols,rows,topo,neigh,rlen1,alpha1,radious1,rlen2,alpha2,radious2,nameO)
 print 'Run %i done'%Run
